9ba8676071b80ec6e7a466c86ddae19ae2b0c9f3
Ayako <ayomori@microsoft.com>
2017-05-23 00:47:29 +0900

    Emotion AI Class Library Update

--- EmotionWeb-NETCore/Controllers/HomeController.cs
+++ EmotionWeb-NETCore/Controllers/HomeController.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-//追加
-using Microsoft.AspNetCore.Http;
+// 追加
 using Microsoft.ProjectOxford.Emotion;
 using Microsoft.ProjectOxford.Emotion.Contract;
-using System.Net.Http;
-using System.Net.Http.Headers;
 
-namespace EmotionWeb_NETCore.Controllers
+namespace EmotionWeb.Controllers
 {
     public class HomeController : Controller
     {
@@ -36,62 +33,64 @@ namespace EmotionWeb_NETCore.Controllers
         [HttpPost]
         public async Task<IActionResult> Result(string url)
         {
-            var responseStatus = "";
+            // 取得可否を設定
+            var res = "";
+            // フォームからの入力(URL)を取得
             ViewData["Url"] = url;
 
-            //Emotion API の Subscription Key をセット
-            //お持ちの Subscription Key を YOUR_SUBSCRIPTION_KEY 部分にコピーしてください
-            const string emotionApiKey = "35d9fbb111674c20b0fccd348cff23b3";
+            // Emotion API の Subscription Key をセット
+            // お持ちの Subscription Key を YOUR_SUBSCRIPTION_KEY 部分にコピーしてください
+            const string eApiKey = "YOUR_SUBSCRIPTION_KEY";
 
             // Emotion API を Call する EmotionServiceClient を生成
-            var emotionServiceClient = new EmotionServiceClient(emotionApiKey);
-            Emotion[] emotionResult = null;
+            var eClient = new EmotionServiceClient(eApiKey);
+            // 結果を取得する配列を定義
+            Emotion[] eResult = null;
 
+            // Emotion API を呼び出して結果を取得
             try
             {
-                emotionResult = await emotionServiceClient.RecognizeAsync(url);
+                eResult = await eClient.RecognizeAsync(url);
             }
             catch (Exception e)
             {
-                emotionResult = null;
+                eResult = null;
             }
 
-            //Call 結果を元に 返答を作成
-            if (emotionResult != null)
+            // 呼び出し結果を元に表示内容を作成
+            if (eResult != null)
             {
                 //表情スコアを取得
                 try
                 {
-                    responseStatus = "結果:";
-                    var emotionScores = emotionResult[0].Scores;
+                    res = "結果:";
+                    var eScores = eResult[0].Scores;
 
                     // 検出された表情スコアをセット
-                    ViewData["Anger"] = Math.Round(emotionScores.Anger, 6).ToString("0.000000");
-                    ViewData["Contempt"] = Math.Round(emotionScores.Contempt, 6).ToString("0.000000");
-                    ViewData["Disgust"] = Math.Round(emotionScores.Disgust, 6).ToString("0.000000");
-                    ViewData["Fear"] = Math.Round(emotionScores.Fear, 6).ToString("0.000000");
-                    ViewData["Happiness"] = Math.Round(emotionScores.Happiness, 6).ToString("0.000000");
-                    ViewData["Neutral"] = Math.Round(emotionScores.Neutral, 6).ToString("0.000000");
-                    ViewData["Sadness"] = Math.Round(emotionScores.Sadness, 6).ToString("0.000000");
-                    ViewData["Surprise"] = Math.Round(emotionScores.Surprise, 6).ToString("0.000000");
+                    ViewData["Anger"] = Math.Round(eScores.Anger, 6).ToString("0.000000");
+                    ViewData["Contempt"] = Math.Round(eScores.Contempt, 6).ToString("0.000000");
+                    ViewData["Disgust"] = Math.Round(eScores.Disgust, 6).ToString("0.000000");
+                    ViewData["Fear"] = Math.Round(eScores.Fear, 6).ToString("0.000000");
+                    ViewData["Happiness"] = Math.Round(eScores.Happiness, 6).ToString("0.000000");
+                    ViewData["Neutral"] = Math.Round(eScores.Neutral, 6).ToString("0.000000");
+                    ViewData["Sadness"] = Math.Round(eScores.Sadness, 6).ToString("0.000000");
+                    ViewData["Surprise"] = Math.Round(eScores.Surprise, 6).ToString("0.000000");
                 }
-
                 catch (Exception e)
                 {
-                    emotionResult = null;
-                    responseStatus = "検出できませんでした";
+                    res = "検出できませんでした";
                 }
-
             }
             else
             {
-                responseStatus = "検出できませんでした";
+                res = "API の呼び出しに失敗しました";
             }
 
-            ViewData["Result"] = responseStatus;
+            // 結果を表示
+            ViewData["Result"] = res;
             return View();
-        }
 
+        }
 
         public IActionResult Error()
         {
